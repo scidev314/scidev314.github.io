11e6f3f5a5192c8a67b9be33a58384b76652d52d
scidev314 <164924620+scidev314@users.noreply.github.com>
2024-11-06 17:26:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ button.addEventListener("click", changeColor);
 
 function changeColor() {
   document.getElementById("ohio").style.color = "green";
+  talk();
+}
+
+function talk() {
   window.alert("why?");
   window.alert("I liked that blue text");
 }
