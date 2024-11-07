720b664572232a10827411bf1d338b63b3770007
scidev314 <164924620+scidev314@users.noreply.github.com>
2024-11-07 08:10:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var button = document.getElementById("test1");
 button.addEventListener("click", changeColor);
 
 function changeColor() {
-  document.getElementById("ohio").style.color = "green";
+  document.getElementById("ohio").style.color = "purple";
   document.getElementById('test2').style.visibility = 'visible';
   talk("why?");
 }
