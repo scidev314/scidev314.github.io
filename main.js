657c330be48e8a9d41c947e28157d432c42b8737
scidev314 <164924620+scidev314@users.noreply.github.com>
2024-11-07 20:23:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-var button = document.getElementById("test1");
-button.addEventListener("click", changeColor);
-
-function changeColor() {
-  document.getElementById("ohio").style.color = "purple";
-  document.getElementById("test2").style.visibility = 'visible';
-  talk("why?");
-}
-
-function talk(tag) {
-  window.alert(tag);
-  window.alert("I liked that blue text");
-  window.alert("frick you");
-}
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 80, 80);
